1a94e15dde3d52e44abb8db12a4d0a48150a8fea
AmamiyaRen666 <65903379+AmamiyaRen666@users.noreply.github.com>
2021-01-05 16:23:20 +0700

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -17,10 +17,10 @@ def list_drive(update,context):
         if button:
             editMessage(msg, reply, button)
         else:
-            editMessage('No result found', reply, button)
+            editMessage('Tidak ada hasil yang ditemukan', reply, button)
 
     except IndexError:
-        sendMessage('send a search key along with command', context.bot, update)
+        sendMessage('kirim kunci pencarian bersama dengan perintah', context.bot, update)
 
 
 list_handler = CommandHandler(BotCommands.ListCommand, list_drive,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
